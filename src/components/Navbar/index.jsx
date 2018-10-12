bcf0d9ef28d1374163672a0e8628aff8dc05bb43
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-10-12 14:41:18 -0400

    added more components

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
   <div className="container">
     <div className="topbar-left">
       <button className="topbar-toggler">☰</button>
-      <a className="topbar-brand" href="index.html">
-        <img className="logo-default" src="assets/img/don.png" alt="logo" />
-        <img className="logo-inverse" src="assets/img/don.png" alt="logo" />
-      </a>
+      <Link className="topbar-brand" to="/">
+        <img className="logo-default" src= {`${process.env.PUBLIC_URL}/assets/img/don.png`} alt="logo" />
+        <img className="logo-inverse" src={`${process.env.PUBLIC_URL}/assets/img/don.png`} alt="logo" />
+      </Link>
     </div>
     <div className="topbar-right">
       <ul className="topbar-nav nav">
@@ -18,7 +18,9 @@ const Navbar = () => {
           <a className="nav-link" href="index.html">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="create-article.html">Write new article</a>
+
+          <Link className="nav-link" to="/articles/create">Write new article</Link>
+
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Hey Garry!
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import * as serviceWorker from './serviceWorker';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import Footer from './components/Footer';
+import CreateArticle from './components/CreateArticle';
+import Login from './components/Login';
 
 
 const Home = () => {
@@ -26,6 +28,9 @@ ReactDOM.render(
       <Route exact path="/" component={Welcome}/>
       <Route path="/about" component={About}/>
       <Route path="/home" component={Home}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/articles/create" component={CreateArticle}/>
+
 
       <Footer />
 
