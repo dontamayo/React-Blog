30d70048ff36ebfd0772d58ddb960b64fd3b474d
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-10-14 21:04:33 -0400

    added more components, login and signup

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 1. npm install react-router react-router-dom
 * react-router is used also for mobile, to view different pages, react-router-dom is used for web browser
 2. https://magic.reactjs.net/htmltojsx.htm
+3. npm install --save indicative --- for validation package
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, withRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
@@ -8,29 +8,45 @@ import Footer from './components/Footer';
 import CreateArticle from './components/CreateArticle';
 import Login from './components/Login';
 import SingleArticle from './components/SingleArticle';
+import Signup from './components/Signup';
 
 
+const Main = withRouter(({location})=> {
+  return (
+    <div>
+
+      {
+        location.pathname !== '/login' && location.pathname !== '/signup' &&
+        <Navbar />
+      }
+
+
+      <Route exact path="/" component={Welcome}/>
+      {/*<Route path="/about" component={About}/>
+      <Route path="/home" component={Home}/>*/}
+      <Route path="/signup" component={Signup}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/article/:slug" component={SingleArticle}/>
+      <Route path="/articles/create" component={CreateArticle}/>
+
+        {
+          location.pathname !== '/login' && location.pathname !== '/signup' &&
+
+          <Footer />
+        }
+
+
+
+    </div>
+  )
+});
 
 
 ReactDOM.render(
 
   <BrowserRouter>
 
-    <div>
-
-      <Navbar />
-
-      <Route exact path="/" component={Welcome}/>
-      {/*<Route path="/about" component={About}/>
-      <Route path="/home" component={Home}/>*/}
-      <Route path="/login" component={Login}/>
-      <Route path="/article/:slug" component={SingleArticle}/>
-      <Route path="/articles/create" component={CreateArticle}/>
-
-
-      <Footer />
-
-    </div>
+    <Main />
 
   </BrowserRouter>
 , document.getElementById('root'));
